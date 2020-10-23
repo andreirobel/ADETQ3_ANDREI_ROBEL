aab1c113e7ff43d15a28e3d3d33d72171a04f961
Lenovo Ideapad A320 <Lenovo Ideapad A320@DESKTOP-10K0GFT>
2020-10-23 20:20:46 +0800

    ADETQ3_ANDREI_ROBEL

---
+++ ADETQ3_ANDREI_ROBEL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_ANDREI_ROBEL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
